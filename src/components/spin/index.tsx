ca36d91c4018d9a9807ae2aa7ebb4b1f56a91a78
Li Hongyin <hongyin163@163.com>
2019-03-19 21:15:15 +0800

    update

--- src/components/spin/index.tsx
+++ src/components/spin/index.tsx
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
 import classNames from 'classnames';
+import React, { Component } from 'react';
 
 interface SpinerProps {
-  tip?: string | React.ReactNode,
-  size?: string
+  type?: string;
+  tip?: string | React.ReactNode;
+  size?: string;
 }
 
 export default class Spiner extends Component<SpinerProps> {
-  render() {
+  public render() {
     const me = this;
     const {
       tip,
-      size = ''
+      size = '',
     } = me.props;
-    let cls = classNames({
+    const cls = classNames({
       'biz-spin': true,
       'biz-spin-sm': size === 'small',
       'biz-spin-lg': size === 'large',
